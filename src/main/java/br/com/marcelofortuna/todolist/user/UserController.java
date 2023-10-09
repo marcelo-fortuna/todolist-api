ab8558acb816ee788bb93995f45e6f25c00ba1a4
Marcelo <90582371+marcelo-fortuna@users.noreply.github.com>
2023-10-09 19:40:56 -0300

    Initial Commit

---
+++ src/main/java/br/com/marcelofortuna/todolist/user/UserController.java
@@ -0,0 +1,26 @@
+package br.com.marcelofortuna.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Modificadores
+ * public
+ * private
+ * protected
+ */
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+    /**
+     * Body 
+     * 
+     */
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel) {
+        System.out.println(userModel.name);
+    }
+}
---
+++ src/main/java/br/com/marcelofortuna/todolist/user/UserModel.java
@@ -0,0 +1,8 @@
+package br.com.marcelofortuna.todolist.user;
+
+public class UserModel {
+    
+    public String username;
+    public String name;
+    public String password;
+}
