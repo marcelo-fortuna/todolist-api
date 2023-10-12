55cc6d71092e6d6155648ba80b004a22a2fac159
Nuts <celofortuna@gmail.com>
2023-10-12 15:11:31 -0300

    Creating task model

---
+++ src/main/java/br/com/marcelofortuna/todolist/task/TaskModel.java
@@ -0,0 +1,42 @@
+package br.com.marcelofortuna.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+/**
+ * ID
+ * User (ID_USER)
+ * Description
+ * TItle
+ * Init Date
+ * End Date
+ * Priority
+ */
+
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String description;
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+
+    private UUID idUser;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
