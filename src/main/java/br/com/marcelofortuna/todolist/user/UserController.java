e576c7b6be272c16e3a8ccf1053559915d42faae
Nuts <celofortuna@gmail.com>
2023-10-12 15:04:07 -0300

    BCrypting password
    + The password is encrypted with BCrypt, which uses hashing as a means of encryption to encrypt before saving to the database

--- src/main/java/br/com/marcelofortuna/todolist/user/UserController.java
+++ src/main/java/br/com/marcelofortuna/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 /**
  * Modificadores
  * public
@@ -28,6 +30,11 @@ public class UserController {
         if (user != null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe.");
         }
+
+        var passwordHashred = BCrypt.withDefaults()
+            .hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
